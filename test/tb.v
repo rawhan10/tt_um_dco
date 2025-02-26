19dc8b2fef42543349ced2acf391679ff318339a
rawhan10 <rohanloni50@gmail.com>
2025-02-26 20:30:29 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,9 +1,12 @@
 `default_nettype none
 `timescale 1ns / 1ps
 
+/* This testbench just instantiates the module and makes some convenient wires
+   that can be driven / tested by the cocotb test.py.
+*/
 module tb ();
 
-  // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
+  // Dump the signals to a VCD file. You can view it with gtkwave.
   initial begin
     $dumpfile("tb.vcd");
     $dumpvars(0, tb);
@@ -14,33 +17,33 @@ module tb ();
   reg clk;
   reg rst_n;
   reg ena;
-  reg [7:0] ui_in;  // Changed from wire to reg
+  wire [7:0] ui_in;
   wire [7:0] uio_in;
+  reg [7:0] ui_in_reg;
+  reg [7:0] uio_in_reg;
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
-  
+
+  // Replace tt_um_example with your module name:
+  tt_um_mac user_project (
+
+      // Include power ports for the Gate Level test:
 `ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
+      .VPWR(1'b1),
+      .VGND(1'b0),
 `endif
 
-  tt_um_dco user_project (
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
-      .ui_in  (ui_in),    
-      .uo_out (uo_out),  
-      .uio_in (uio_in),  
-      .uio_out(uio_out),  
-      .uio_oe (uio_oe),  
-      .ena    (ena),      
-      .clk    (clk),      
-      .rst_n  (rst_n)     
+      .ui_in  (ui_in),    // Dedicated inputs
+      .uo_out (uo_out),   // Dedicated outputs
+      .uio_in (uio_in),   // IOs: Input path
+      .uio_out(uio_out),  // IOs: Output path
+      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+      .ena    (ena),      // enable - goes high when design is selected
+      .clk    (clk),      // clock
+      .rst_n  (rst_n)     // not reset
   );
-
-  reg [7:0] dco_code;  
+ reg [7:0] dco_code;  
 
   always @(*) ui_in = dco_code; // Fixed wire assignment issue
   always #10 clk = ~clk;
@@ -66,6 +69,7 @@ module tb ();
     
     #2000 $finish;
   end
+  
   initial begin
     $monitor("Time=%0d | ui_in=%b, uio_in=%b | uo_out=%b", $time, ui_in, uio_in, uo_out);
   end
