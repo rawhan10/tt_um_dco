58727762cddc799eb01ea266e29456b2546dc66e
rawhan10 <rohanloni50@gmail.com>
2025-03-01 23:57:06 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -19,9 +19,9 @@ async def test_project(dut):
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 0
 
     dut._log.info("Test project behavior")
 
