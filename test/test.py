bad8dafa5e5d849921509f701eaad26fed23fc4a
rawhan10 <rohanloni50@gmail.com>
2025-03-02 21:46:15 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -22,13 +22,16 @@ async def test_project(dut):
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 1
+
+    dut.ui_in.value = 1
+
     await Timer(20, units="ns")
     dut.rst_n.value = 0
 
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    dut.ui_in.value = 1
+    
     # dut.uio_in.value = 30
 
     # Wait for one clock cycle to see the output values
